1c7599f8e246ae4dd7ad73df8adbdbfe9d5d0c01
Piyal Hossein <piyal13133@gmail.com>
2024-10-18 23:12:53 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import Navber from './components/Navber.vue';
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  
+  <Navber />
+  <RouterView />
+  
+</template>
---
+++ src/components/BackButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+    import { defineProps } from 'vue';
+    import { RouterLink } from 'vue-router';
+    defineProps({
+        link: {
+            type: String,
+            default: '/jobs'
+        },
+        text: {
+            type: String,
+            default: 'Job Listings'
+        },
+    })
+
+</script>
+
+<template>
+    <section>
+      <div class="container m-auto py-6 px-6">
+        <RouterLink
+          :to="link"
+          class="text-green-500 hover:text-green-600 flex items-center"
+        >
+          <i class="pi pi-arrow-circle-left mr-2 text-xl"></i> Back To {{ text }}
+        </RouterLink>
+      </div>
+    </section>
+</template>
